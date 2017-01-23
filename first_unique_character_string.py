14d3b2f125bc05ae37fd2d0a1686538901fa7f19
bridgetree <rahultach@gmail.com>
2017-01-23 14:21:12 -0500

    Create first_unique_character_string.py

---
+++ first_unique_character_string.py
@@ -0,0 +1,18 @@
+def firstUniqChar(self, s):
+          if s == "":
+              return -1
+          dict = {}
+          for i in s:
+              if i in dict.keys():
+                  dict[i] += 1
+              else:
+                  dict[i] = 1
+
+          max = 0
+          print dict
+          cnt = 0
+          for i in s:
+              if dict[i] == 1:
+                  return cnt
+              cnt += 1
+          return -1
