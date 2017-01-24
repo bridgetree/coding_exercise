0cb54aa84389b11999aa94a2f9c0b1c027f47cb3
bridgetree <rahultach@gmail.com>
2017-01-24 06:38:04 -0500

    Create reverse_linked_list.py

---
+++ reverse_linked_list.py
@@ -0,0 +1,22 @@
+# Iteration
+
+def reverseList(self, head):
+    prev = None
+    while head:
+        curr = head
+        head = head.next
+        curr.next = prev
+        prev = curr
+    return prev
+    
+# Recursion
+
+def reverseList(self, head):
+    return self._reverse(head)
+
+def _reverse(self, node, prev=None):
+    if not node:
+        return prev
+    n = node.next
+    node.next = prev
+    return self._reverse(n, node)
