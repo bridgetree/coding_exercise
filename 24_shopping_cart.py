690a1b737f426168a396e8f312058df701d8c602
bridgetree <rahultach@gmail.com>
2017-01-28 07:07:22 -0500

    Update 24_shopping_cart.py

--- 24_shopping_cart.py
+++ 24_shopping_cart.py
@@ -1,12 +1,9 @@
 class ShoppingCart(object):
-    """Creates shopping cart objects
-    for users of our fine website."""
     items_in_cart = {}
     def __init__(self, customer_name):
         self.customer_name = customer_name
 
     def add_item(self, product, price):
-        """Add product to the cart."""
         if not product in self.items_in_cart:
             self.items_in_cart[product] = price
             print product + " added."
@@ -14,7 +11,6 @@ class ShoppingCart(object):
             print product + " is already in the cart."
 
     def remove_item(self, product):
-        """Remove product from the cart."""
         if product in self.items_in_cart:
             del self.items_in_cart[product]
             print product + " removed."
