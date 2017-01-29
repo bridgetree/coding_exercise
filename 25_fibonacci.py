f3eab295cac1ac9588c104b237d44c39e4d586c0
bridgetree <rahultach@gmail.com>
2017-01-29 05:28:11 -0500

    Update 25_fibonacci.py

--- 25_fibonacci.py
+++ 25_fibonacci.py
@@ -1,7 +1,19 @@
 def fib(x):
-        if x==0:
-            return 0
-        if x==1:
-            return 1
+    if x==0:
+        return 0
+    if x==1:
+        return 1
 
-        return fib(x-1) + fib(x-2)
+    return fib(x-1) + fib(x-2)
+
+# Dynamic Programming
+
+# finding fibonacci numbers using dynamic programming
+
+mem = {0: 0, 1: 1}
+def fib(x):
+    if x in mem:
+        return mem[x]
+    r = fib(x-1) + fib(x-2)
+     mem[x] = r
+     return r
